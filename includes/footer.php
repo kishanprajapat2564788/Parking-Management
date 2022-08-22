2dfa7d577ce0540844010a3fa1d306c505f14763
Kishan Prajapat <47397967+kishanprajapat2564788@users.noreply.github.com>
2022-08-22 12:22:46 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                    <div class="col-sm-6">
+                        Copyright &copy; 2021 KISHAN
+                    </div>
+                    <div class="col-sm-6 text-right">
+                     Parking Management System
+                    </div>
+                </div>
+            </div>
+        </footer>
